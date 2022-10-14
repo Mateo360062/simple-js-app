eb70f533e20fce8bbded239184d65b49bda352dd
Mateo360062 <114286178+Mateo360062@users.noreply.github.com>
2022-10-14 11:50:11 +0200

    Pokemon name/height loop
    Making a for loop to display the names and height of pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,13 @@
 let pokemonList = [
-  {name: 'Gastly', height: 3, type: ['ghost','poison']},
+  {name: 'Gastly', height: 3, type: ['ghost', 'poison']},
   {name: 'Milotic', height: 9, type: ['dragon', 'water']},
   {name: 'Steelix', height: 8, type: ['steel', 'rock']}
 ]
-for (let i = 0; let i < pokemonList.length; i++){
-  if (pokemonList[i].height <= 8){
-    document.write('${pokemonList[i].name}:height(${pokemonList[i].height}) 
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height <= 8) {
+    document.write(`<br>${pokemonList[i].name}(height:${pokemonList[i].height})</br>`)
+  }
+  else if (pokemonList[i].height > 8) {
+    document.write(`<br>${pokemonList[i].name}(height:${pokemonList[i].height}) - Wow, that's big!<br>`)
   }
 }
