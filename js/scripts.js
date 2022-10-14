73cbf09d38c65dcac39fa0ae5c939ab24943e4cf
Mateo360062 <114286178+Mateo360062@users.noreply.github.com>
2022-10-14 10:52:18 +0200

    Writing name and height
    Making a for loop to display name and ´height of pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,8 @@ let pokemonList = [
   {name: 'Milotic', height: 9, type: ['dragon', 'water']},
   {name: 'Steelix', height: 8, type: ['steel', 'rock']}
 ]
+for (let i = 0; let i < pokemonList.length; i++){
+  if (pokemonList[i].height <= 8){
+    document.write('${pokemonList[i].name}:height(${pokemonList[i].height}) 
+  }
+}
