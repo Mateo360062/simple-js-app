7ec4228af1891b6a51602a1923c66e8fc829dc18
Mateo360062 <114286178+Mateo360062@users.noreply.github.com>
2022-10-14 10:36:46 +0200

    Reversing changes
    Reversing the changes I made when testing the code

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,3 @@ let pokemonList = [
   {name: 'Milotic', height: 9, type: ['dragon', 'water']},
   {name: 'Steelix', height: 8, type: ['steel', 'rock']}
 ]
-
-  let poke = 'whatever';
-  let mon = 'whatevs';
