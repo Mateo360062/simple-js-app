13cf801f57586e5ceb47ca371af08f4da8ab7afa
Mateo360062 <114286178+Mateo360062@users.noreply.github.com>
2022-10-18 13:54:01 +0200

    Adding functions in IIFE
    Adding add function to add new pokemon on the condition of input being an object with the same keys as the other ones.
    Adding a function for retrieving the pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,29 @@
-let pokemonList = [
-  {name: 'Gastly', height: 3, type: ['ghost', 'poison']},
-  {name: 'Milotic', height: 9, type: ['dragon', 'water']},
-  {name: 'Steelix', height: 8, type: ['steel', 'rock']}
-]
-pokemonList.forEach(function(pokemon){
+let pokemonRepository = (function(){
+   let pokemonList = [
+    {name: 'Gastly', height: 3, type: ['ghost', 'poison']},
+    {name: 'Milotic', height: 9, type: ['dragon', 'water']},
+    {name: 'Steelix', height: 8, type: ['steel', 'rock']}
+  ]
+  return {
+    getAll: function(){
+      return pokemonList
+    },
+    add: function(pokemon){
+      if(typeof pokemon === Object && 
+        Object.keys(pokemon) === Object.keys(pokemonRepository.getAll()[0])){
+          pokemonList.push(pokemon)}
+      }
+    }
+  }
+)()
+
+pokemonRepository.add({name:'Gengar',height:4, type: 'water'})
+document.write(JSON.stringify(pokemonRepository.getAll()))
+pokemonRepository.getAll().forEach(function(pokemon){
   if(pokemon.height <= 8){
-    document.write(`${pokemon.name}(height: ${pokemon.height})<br>`)
+    document.write(`<br>${pokemon.name}(height: ${pokemon.height})<br>`)
   }else if(pokemon.height > 8){
-  document.write(`${pokemon.name}(height: ${pokemon.height}) - That's big!<br>`)
+  document.write(`<br>${pokemon.name}(height: ${pokemon.height}) - That's big!<br>`)
   }
   
 })
@@ -23,18 +39,3 @@ function divide(dividend,divisor){
 }
 divide(2,1);
 console.log(divide(4,2));
-let pokemonRepository = (function(){
-  let pokemonList = ['Pikachu'];
-  return {
-    add: function(pokemon){
-      pokemonList.push(pokemon)
-    },
-    getAll: function(){
-      return pokemonList
-    }
-  }
-})();
-document.write(pokemonRepository.getAll());
-pokemonRepository.add('Charizard');
-document.write(pokemonRepository.getAll());
-document.write(pokemonList);
