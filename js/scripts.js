3867723b21efba8f22e50f4442365be33786fee5
Mateo360062 <114286178+Mateo360062@users.noreply.github.com>
2022-10-18 12:05:33 +0200

    Replacing loop with forEach()
    Refactoring code to use the forEach() method to go through my pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,38 @@ let pokemonList = [
   {name: 'Milotic', height: 9, type: ['dragon', 'water']},
   {name: 'Steelix', height: 8, type: ['steel', 'rock']}
 ]
-for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height <= 8) {
-    document.write(`<br>${pokemonList[i].name}(height:${pokemonList[i].height})</br>`)
+pokemonList.forEach(function(pokemon){
+  if(pokemon.height <= 8){
+    document.write(`${pokemon.name}(height: ${pokemon.height})<br>`)
+  }else if(pokemon.height > 8){
+  document.write(`${pokemon.name}(height: ${pokemon.height}) - That's big!<br>`)
   }
-  else if (pokemonList[i].height > 8) {
-    document.write(`<br>${pokemonList[i].name}(height:${pokemonList[i].height}) - Wow, that's big!<br>`)
+  
+})
+
+
+function divide(dividend,divisor){
+  if(divisor === 0){
+    return document.write('You\'d tried to divide by 0!')
   }
+  else{
+    return dividend / divisor
+  } 
 }
+divide(2,1);
+console.log(divide(4,2));
+let pokemonRepository = (function(){
+  let pokemonList = ['Pikachu'];
+  return {
+    add: function(pokemon){
+      pokemonList.push(pokemon)
+    },
+    getAll: function(){
+      return pokemonList
+    }
+  }
+})();
+document.write(pokemonRepository.getAll());
+pokemonRepository.add('Charizard');
+document.write(pokemonRepository.getAll());
+document.write(pokemonList);
