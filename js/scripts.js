4f60fd849931e0799a09b1c8d04a83b0ce4102b2
Mateo360062 <114286178+Mateo360062@users.noreply.github.com>
2022-10-20 11:28:30 +0200

    Adding event listener to IIFE
    Creating buttons for pokemon and adding event listeners

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,48 @@
 let pokemonRepository = (function(){
-   let pokemonList = [
+  let pokemonList = [
     {name: 'Gastly', height: 3, type: ['ghost', 'poison']},
     {name: 'Milotic', height: 9, type: ['dragon', 'water']},
     {name: 'Steelix', height: 8, type: ['steel', 'rock']}
   ]
-  return {
-    getAll: function(){
-      return pokemonList
-    },
-    add: function(pokemon){
-      if(typeof pokemon === Object && 
-        Object.keys(pokemon) === Object.keys(pokemonRepository.getAll()[0])){
-          pokemonList.push(pokemon)}
+  function getAll(){
+    return pokemonList
+  }
+  function add(pokemon){
+    if(typeof pokemon === Object && 
+      Object.keys(pokemon) === Object.keys(pokemonRepository.getAll()[0])){
+        pokemonList.push(pokemon)}
+      }
+      function addListItem(pokemon){
+        let unorderedList = document.querySelector('.pokemon-list');
+        let listItem = document.createElement('li');
+        let button = document.createElement('button');
+        button.innerText = pokemon.name;
+        button.addEventListener('click', showDetails);
+        button.classList.add('selected');
+        listItem.appendChild(button);
+        unorderedList.appendChild(listItem);
+      }
+      function showDetails(pokemon){
+       console.log(pokemon)
+      }
+      return {
+        getAll: getAll,
+        add: add,
+        addListItem: addListItem,
+        showDetails: showDetails
       }
     }
-  }
-)()
+    )();
+    
+    pokemonRepository.addListItem({name:'Gengar',height:4, type: 'water'})
+    document.write(JSON.stringify(pokemonRepository.getAll()))
+    pokemonRepository.getAll().forEach(function(pokemon){
+      pokemonRepository.addListItem(pokemon);
+    })
 
-pokemonRepository.add({name:'Gengar',height:4, type: 'water'})
-document.write(JSON.stringify(pokemonRepository.getAll()))
-pokemonRepository.getAll().forEach(function(pokemon){
-  if(pokemon.height <= 8){
-    document.write(`<br>${pokemon.name}(height: ${pokemon.height})<br>`)
-  }else if(pokemon.height > 8){
-  document.write(`<br>${pokemon.name}(height: ${pokemon.height}) - That's big!<br>`)
-  }
-  
-})
-
-
-function divide(dividend,divisor){
-  if(divisor === 0){
-    return document.write('You\'d tried to divide by 0!')
-  }
-  else{
-    return dividend / divisor
-  } 
-}
-divide(2,1);
-console.log(divide(4,2));
+    
+    
+    
+    
+    
+    
